822747516bf5a4a6abdab32bf9566ae285b2215d
grogu27 <124275275+grogu27@users.noreply.github.com>
2025-10-15 15:09:01 +0700

    Update button.c

--- lab3/button.c
+++ lab3/button.c
@@ -16,40 +16,6 @@ uint8_t read_button_debounced(gpio_num_t pin)
     return (first == second) ? first : read_button_debounced(pin);
 }
 
-// void app_main(void)
-// {
-//     gpio_set_direction(LED, GPIO_MODE_OUTPUT);
-//     gpio_set_direction(BUTTON, GPIO_MODE_INPUT);
-//     gpio_set_pull_mode(BUTTON, GPIO_FLOATING); 
-
-//     int delay_ms = 500; // 1 Гц мигание
-//     int led_state = 0;
-//     uint8_t button;
-//     uint8_t last_button = 1;
-//     while (1) {
-//         button = gpio_get_level(BUTTON);
-//         //button = read_button_debounced(BUTTON);
-//         printf("GPIO state is %d\n", button);
-
-//         if ((last_button == 1 && button == 0) || (last_button == 0 && button == 1)) 
-//             printf("Кнопка была нажата!\n");
-        
-//         // Обновляем состояние
-//         last_button = button;
-
-//         if (button == 0) 
-//           delay_ms = 100; // 5 Гц
-//         else
-//             delay_ms = 500; // 1 Гц
-
-//         led_state = !led_state;
-//         gpio_set_level(LED, led_state);
-
-//         vTaskDelay(delay_ms / portTICK_PERIOD_MS);
-//     }
-// }
-
-// мк видит только краткий переход 
 void app_main(void)
 {
     gpio_set_direction(LED, GPIO_MODE_OUTPUT);
@@ -57,26 +23,60 @@ void app_main(void)
     gpio_set_pull_mode(BUTTON, GPIO_FLOATING); 
 
     int delay_ms = 500; // 1 Гц мигание
+    int led_state = 0;
     uint8_t button;
     uint8_t last_button = 1;
     while (1) {
+        //button = gpio_get_level(BUTTON);
+        button = read_button_debounced(BUTTON);
+        printf("GPIO state is %d\n", button);
 
-      //button = gpio_get_level(BUTTON);
-      button = read_button_debounced(BUTTON);
-      printf("GPIO state is %d\n", button);
-      if ((last_button == 1 && button == 0) || (last_button == 0 && button == 1)) 
-        printf("Кнопка была нажата!\n");
-          //Обновляем состояние
-          last_button = button;
+        if ((last_button == 1 && button == 0) || (last_button == 0 && button == 1)) 
+            printf("Кнопка была нажата!\n");
+        
+        // Обновляем состояние
+        last_button = button;
 
-      if(button)
-        gpio_set_level(LED, 0);
-      else
-        gpio_set_level(LED, 1);
-      
-      vTaskDelay(delay_ms / portTICK_PERIOD_MS);
-      }
+        if (button == 0) 
+          delay_ms = 100; // 5 Гц
+        else
+            delay_ms = 500; // 1 Гц
+
+        led_state = !led_state;
+        gpio_set_level(LED, led_state);
+
+        vTaskDelay(delay_ms / portTICK_PERIOD_MS);
+    }
 }
+
+// мк видит только краткий переход 
+// void app_main(void)
+// {
+//     gpio_set_direction(LED, GPIO_MODE_OUTPUT);
+//     gpio_set_direction(BUTTON, GPIO_MODE_INPUT);
+//     gpio_set_pull_mode(BUTTON, GPIO_FLOATING); 
+
+//     int delay_ms = 500; // 1 Гц мигание
+//     uint8_t button;
+//     uint8_t last_button = 1;
+//     while (1) {
+
+//       button = gpio_get_level(BUTTON);
+//       //button = read_button_debounced(BUTTON);
+//       printf("GPIO state is %d\n", button);
+//       if ((last_button == 1 && button == 0) || (last_button == 0 && button == 1)) 
+//         printf("Кнопка была нажата!\n");
+//           //Обновляем состояние
+//           last_button = button;
+
+//       if(button)
+//         gpio_set_level(LED, 0);
+//       else
+//         gpio_set_level(LED, 1);
+      
+//       vTaskDelay(delay_ms / portTICK_PERIOD_MS);
+//       }
+// }
 
 // через внутренний под регистр, он подтягивает пин к 1 когда кнопка не замкнута
 // void app_main(void)
@@ -100,7 +100,6 @@ void app_main(void)
 //         printf("Кнопка была нажата!\n");
 //       } 
         
-//           //Обновляем состояние
 //           last_button = button;
 
 //       if(fast_mod)
