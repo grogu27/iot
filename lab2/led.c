b539035d1cded02cb533f3e1951ee7a6e757449a
grogu27 <124275275+grogu27@users.noreply.github.com>
2025-10-15 14:13:03 +0700

    Create led.c

---
+++ lab2/led.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+
+
+#define LED GPIO_NUM_14
+
+void app_main(void)
+{
+
+  gpio_set_direction(LED, GPIO_MODE_OUTPUT);
+  uint32_t led_on = 0;
+  while (true)
+  {
+    led_on = !led_on;
+    gpio_set_level(LED, led_on);
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+  }
+}
